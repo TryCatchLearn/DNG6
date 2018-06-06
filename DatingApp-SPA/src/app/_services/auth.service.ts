ee150bf53abd7da59c31c46c2b10ef2d21261a9a
trycatchlearn <trycatchlearn@outlook.com>
2018-06-06 13:38:02 +0100

    End of section 4

---
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  baseUrl = 'http://localhost:5000/api/auth/';
+  userToken: any;
+
+constructor(private http: HttpClient) { }
+
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'login', model, this.requestOptions())
+      .pipe(
+        map((response: any) => {
+          const user = response;
+          if (user) {
+            localStorage.setItem('token', user.tokenString);
+            this.userToken = user.tokenString;
+          }
+        })
+      );
+  }
+
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'register', model, this.requestOptions());
+  }
+
+  private requestOptions() {
+    const headers = new HttpHeaders();
+    headers.set('Content-type', 'application/json');
+    return {headers};
+  }
+}
