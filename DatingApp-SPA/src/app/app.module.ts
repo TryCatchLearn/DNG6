e9a03a304d3cb073a213df24c4adac9468111d74
trycatchlearn <trycatchlearn@outlook.com>
2018-06-06 19:56:06 +0100

    End of section 6

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -11,11 +11,25 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AlertifyService } from './_services/alertify.service';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
+import { AuthGuard } from './_guards/auth.guard';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule, BsDropdownModule.forRoot()],
-  providers: [AuthService, ErrorInterceptorProvider, AlertifyService],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
+    MemberListComponent,
+    ListsComponent,
+    MessagesComponent
+  ],
+  imports: [BrowserModule, HttpClientModule, FormsModule, BsDropdownModule.forRoot(), RouterModule.forRoot(appRoutes)],
+  providers: [AuthService, ErrorInterceptorProvider, AlertifyService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -10,27 +11,32 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   login() {
-    this.authService.login(this.model).subscribe(data => {
-      this.alertify.success('logged in successfully');
-    }, error => {
-      this.alertify.error(error);
-    });
+    this.authService.login(this.model).subscribe(
+      data => {
+        this.alertify.success('logged in successfully');
+      },
+      error => {
+        this.alertify.error('Failed to login');
+      },
+      () => {
+        this.router.navigate(['/members']);
+      }
+    );
   }
 
   logout() {
     this.authService.userToken = null;
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+    this.router.navigate(['/home']);
   }
 
   loggedIn() {
     return this.authService.loggedIn();
   }
-
 }
