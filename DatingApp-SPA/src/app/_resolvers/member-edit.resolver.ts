f1aa44d295491ee520612e779a181faeb14b14a7
trycatchlearn <trycatchlearn@outlook.com>
2018-06-08 19:38:16 +0100

    End of module 10

---
+++ DatingApp-SPA/src/app/_resolvers/member-edit.resolver.ts
@@ -0,0 +1,28 @@
+import { Resolve, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { User } from '../_models/User';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { UserService } from '../_services/user.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { catchError } from 'rxjs/operators';
+import { AuthService } from '../_services/auth.service';
+
+@Injectable()
+export class MemberEditResolver implements Resolve<User> {
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private alertify: AlertifyService,
+    private authService: AuthService
+  ) {}
+
+  resolve(route: ActivatedRouteSnapshot): Observable<User> {
+    return this.userService.getUser(this.authService.decodedToken.nameid).pipe(
+      catchError(error => {
+        this.alertify.error('Problem retrieving data');
+        this.router.navigate(['/members']);
+        return of(null);
+      })
+    );
+  }
+}
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -23,6 +23,9 @@ import { JwtModule } from '@auth0/angular-jwt';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 export function tokenGetter() {
   return localStorage.getItem('token');
@@ -38,7 +41,8 @@ export function tokenGetter() {
     ListsComponent,
     MessagesComponent,
     MemberCardComponent,
-    MemberDetailComponent
+    MemberDetailComponent,
+    MemberEditComponent
   ],
   imports: [
     BrowserModule,
@@ -62,7 +66,9 @@ export function tokenGetter() {
     AlertifyService,
     AuthGuard,
     MemberDetailResolver,
-    MemberListResolver
+    MemberListResolver,
+    MemberEditResolver,
+    PreventUnsavedChanges
   ],
   bootstrap: [AppComponent]
 })
